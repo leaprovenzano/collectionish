9946175c1c4113df1d07bdd9cd156b03da4f8631
lea <leaprovenzano@gmail.com>
2019-12-25 09:26:54 +0000

    update is_valid_identifier test to validate through ast parse

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,18 +1,28 @@
 import ast
 
-import pytest
-
 import hypothesis.strategies as st
 from hypothesis import given
 
 from more_containers.utils import is_valid_identifier
 
 
+def validate_identifier_as_ast(s: str):
+    parsed = ast.parse(f'{s}=1', mode='single')
+    assert len(parsed.body) == 1
+    body = parsed.body[0]
+    assert isinstance(body, ast.Assign)
+    assert body.lineno == 1
+    assert body.col_offset == 0
+
+    assert body.value.n == 1
+
+    assert len(body.targets) == 1
+    name = body.targets[0]
+    assert isinstance(name, ast.Name)
+    assert name.id == s
+
+
 @given(st.text())
 def test_is_valid_identifier(s):
-    statement = f'{s}=1'
     if is_valid_identifier(s):
-        ast.literal_eval(statement)
-    else:
-        with pytest.raises(SyntaxError):
-            ast.literal_eval(statement)
+        validate_identifier_as_ast(s)
