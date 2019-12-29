425463744bc329b423fe60ee6c6fb759374ac413
lea <leaprovenzano@gmail.com>
2019-12-29 18:48:26 +0000

    add rsetattr op

--- src/more_containers/ops.py
+++ src/more_containers/ops.py
@@ -31,3 +31,37 @@ def rgetattr(obj: Any, *keys: Iterable[str]):
 
     """
     return reduce(getattr, keys, obj)  # type: ignore
+
+
+def rsetattr(obj: Any, keys: Iterable[str], value: Any):
+    """A recursive version of :function:`python:setattr`.
+
+    Example:
+
+        Again we'll use :class:`python:types.SimpleNamespace` as a standin for \
+        anything with attibute access:
+
+        >>> from types import SimpleNamespace
+        >>> from more_containers.ops import rgetattr
+        >>>
+        >>> thing = SimpleNamespace()
+
+        with only one key it works like normal :function:`python:setattr`:
+
+        >>> rsetattr(thing, ['a'], 1)
+        >>> thing
+        namespace(a=1)
+        >>> rsetattr(thing, ['b'], SimpleNamespace(ba= 1, bb= 2))
+        >>> thing
+        namespace(a=1, b=namespace(ba=1, bb=2))
+
+        with multiple ``rsetattr`` can update a nested value:
+
+        >>> rsetattr(thing, ('b', 'ba'), 2)
+        >>> thing
+        namespace(a=1, b=namespace(ba=2, bb=2))
+
+    """
+    *keys, last = keys
+    parent = rgetattr(obj, *keys)
+    setattr(parent, last, value)
