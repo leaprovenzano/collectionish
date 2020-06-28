436e1f8231bebaca3d9a323d038a43670d025245
lea <leaprovenzano@gmail.com>
2020-06-28 18:46:11 +0100

    bump version to 0.2.1

--- src/collectionish/__init__.py
+++ src/collectionish/__init__.py
@@ -5,7 +5,7 @@
 
 __author__ = """Lea Provenzano"""
 __email__ = 'leaprovenzano@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from ._uniquetuple import UniqueTuple
 from ._attydict import AttyDict
